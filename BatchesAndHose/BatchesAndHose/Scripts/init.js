a92a97bfee8c334835d98bc2cdcac6ae0b1f347e
David Algar <david.algar@mindbodyonline.com>
2013-01-29 16:41:07 -0800

    add init js

---
+++ BatchesAndHose/BatchesAndHose/Scripts/init.js
@@ -0,0 +1,24 @@
+﻿// Declare a proxy to reference the hub. 
+var chat = $.connection.chatHub;
+
+var canvasWidth = WIDTH;
+var canvasHeight = HEIGHT;
+var playerHeight = 50;
+
+var WIDTH = 1024;		// Canvas width
+var HEIGHT = 600;		// Canvas height
+var FPS = 24;			// Framerate
+var rateLimit = 1;
+var currentWait = 0;
+
+
+var canvas;				// Canvas element
+var g; 					// Graphics context
+var players = [];		// Contains all the active player objects
+var n = 0;				// Index of the player being controlled by this client
+
+var projectiles = [];
+var asteroids = [];
+
+var mouseX;
+var mouseY;
