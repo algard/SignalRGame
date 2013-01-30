68141ad63ddafdc9018caccb5d106a1f3c8f6567
girl <wysowl88@comcast.net>
2013-01-29 16:27:24 -0800

    add Flickr result urls

--- BatchesAndHose/BatchesAndHose/ChatHub.cs
+++ BatchesAndHose/BatchesAndHose/ChatHub.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using BatchesAndHose.Models;
 using Microsoft.AspNet.SignalR;
+using System.Xml;
 
 
 public class ChatHub : Hub
@@ -44,22 +45,33 @@ public class ChatHub : Hub
     /*
      *  Add a new player to the game
      */
-    public void AddNewPlayer(string name)
+    public void AddNewPlayer(string name, string image)
     {
         var x = RandomLocation(50, CanvasWidth) - PlayerWidth;
         var y = CanvasHeight;
 
-        var newPlayer = new Player(name, x, CanvasWidth);
+		//get photos from Flickr
+		List<string> urlArray = new List<string>();
+
+		XmlDocument urlDoc = new XmlDocument();
+		urlDoc.Load("http://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=8531f340cce6465f6570d9933698ec52&text=" + image);
+
+		foreach (XmlElement photo in urlDoc.SelectNodes("/rsp/photos/photo"))
+		{
+			urlArray.Add("http://farm" + photo.GetAttribute("farm") + ".staticflickr.com/" + photo.GetAttribute("server") + "/" + photo.GetAttribute("id") + "_" + photo.GetAttribute("secret") + "_s.jpg");
+		}
+
+        var newPlayer = new Player(name, image, urlArray, x, CanvasWidth);
 
         // send the list of players to this new player
         foreach (var player in _players)
         {
-            Clients.Caller.addPlayer(player.Name, player.LocationX);
+			Clients.Caller.addPlayer(player.Name, player.Image, player.ImageURLs, player.LocationX);
         }
         _players.Add(newPlayer);
 
         //notify other players that a new player has been added
-        Clients.All.addPlayer(name, x, y);
+		Clients.All.addPlayer(name, image, urlArray, x, y);
     }
 
     private static int RandomLocation(int startX, int endX)
@@ -73,3 +85,4 @@ public class ChatHub : Hub
 
     }
 }
+
